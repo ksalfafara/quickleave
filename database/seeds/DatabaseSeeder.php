5cbb272d4c9ba37f40edbbac4a96a6ffc4f69538
Kervi Alfafara <alfafarakervi@gmail.com>
2015-02-22 11:47:32 +0800

    added Teams Model

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+
+class DatabaseSeeder extends Seeder {
+
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
+		Model::unguard();
+
+		$this->call('TeamTableSeeder');
+	}
+
+}
+
+class TeamTableSeeder extends Seeder {
+
+	public function run() {
+
+		for($i = 0; $i < 5; $i++) {
+
+			$team = new Team;
+			$team->name = 'Team '.$i;
+			$team->code = 'Code '.$i;
+			$team->save();
+
+		}
+	}
+}
